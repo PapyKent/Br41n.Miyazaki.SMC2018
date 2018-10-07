7d7bb3fb1449c52aaefde4e4dffc79111c40e0a9
PapyKent <kentdu38@gmail.com>
2018-10-07 19:38:15 +0900

    gound prefabs + mov plateform
    Co-Authored-By: Quentin FERRE <papykent@users.noreply.github.com>

---
+++ Br41n-SMC/Assets/Scripts/PlatformMov.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMov : MonoBehaviour {
+
+    Vector3 referencePostion;
+    public bool upNdown = true;
+    bool positiveMov = false;
+    public float movDistance = 2.0f;
+    public float speed = 2;
+    public Vector2 direction = new Vector2(0, 1);
+ 
+    // Use this for initialization
+    void Start () {
+        referencePostion = this.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movement = new Vector3(0, 0);
+
+        if (upNdown)
+        {
+            if (Mathf.Abs( referencePostion.y - this.transform.position.y) > movDistance)
+                positiveMov = !positiveMov;
+
+            if (positiveMov)
+                direction.y = -1 * direction.y;
+
+             movement = new Vector2(direction.x*speed, direction.y*speed);
+        }
+        else
+        {
+
+            movement = new Vector3(speed * direction.x, 0);
+        }
+
+
+        movement *= Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
---
+++ Br41n-SMC/Assets/Scripts/ScrollingMov.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingMov : MonoBehaviour {
+
+    /// <summary>
+    /// Scrolling speed
+    /// </summary>
+    public Vector2 speed = new Vector2(2, 2);
+
+    /// <summary>
+    /// Moving direction
+    /// </summary>
+    public Vector2 direction = new Vector2(-1, 0);
+
+    /// <summary>
+    /// Movement should be applied to camera
+    /// </summary>
+    public bool isLinkedToCamera = false;
+
+    public int distToCam = 10;
+        
+
+    void Update()
+    {
+        // Movement
+        Vector3 movement = new Vector3(
+          speed.x * direction.x,
+          speed.y * direction.y,
+          0);
+
+        movement *= Time.deltaTime;
+        transform.Translate(movement);
+
+        // Move the camera
+        if (isLinkedToCamera)
+        {
+            Camera.main.transform.Translate(movement);
+        }
+
+        //respawn
+        if (this.GetComponent<Transform>().position.x < -distToCam)
+        {
+            float yBias = Random.Range(-1, 5);
+            float xBias = Random.Range(-5, 5);
+            this.GetComponent<Transform>().position = new Vector2 (Camera.main.transform.position.x * -1 + xBias + distToCam,  yBias);
+        }
+    }
+}
+
